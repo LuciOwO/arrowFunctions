f4541e1df7c1bc1bfe8a292ef14ddc3bfed37ae0
root <root@LAPTOP-15OOEI67.localdomain>
2022-04-08 12:42:19 -0400

    Adding arrow function excercise to github

---
+++ ArrowFunction.js
@@ -0,0 +1,20 @@
+/*Refactor the code to use two arrow functions. Turn it into a one-liner.
+function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+}*/
+const double = arr => { return arr.map(val => val * 2) }
+
+/*Replace ALL functions with arrow functions:
+function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+}*/
+
+const squareAndFindEvens = numbers => numbers.map((num) => num ** 2).filter((square) =>square % 2 === 0);
